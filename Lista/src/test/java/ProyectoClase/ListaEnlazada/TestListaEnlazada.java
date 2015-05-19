900486ae229db4eb2ea5d5986b466ad0338297cd
Rocío <rocioarrebola95@hotmail.com>
2015-05-19 20:30:37 +0200

    TestListaEnlazada.java

---
+++ Lista/src/test/java/ProyectoClase/ListaEnlazada/TestListaEnlazada.java
@@ -0,0 +1,15 @@
+package ProyectoClase.ListaEnlazada;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestListaEnlazada {
+  
+  ListaEnlazada<>
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
